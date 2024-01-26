59dc42e93b539880aceee0234436f7f38a3d13ee
dakotaSeeker <dsicher@kent.edu>
2024-01-26 12:10:53 -0500

    updated backend.go to use hello world from echo example here https://pkg.go.dev/github.com/labstack/echo/v4#section-readme

--- cmd/backend.go
+++ cmd/backend.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	//"github.com/jackc/pgx/v5"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
