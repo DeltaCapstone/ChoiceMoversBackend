94d574a4ab5ad30718ca612aad155fdf24221c18
dakotaSeeker <dsicher@kent.edu>
2024-03-06 19:38:32 -0500

    preping for adding sessions and db changes changed type of employee in the JobResponse struct

---
+++ database/migration/000002_update1.up.sql
@@ -0,0 +1,7 @@
+--add number of workers to job
+--add priority level to employees
+--rename "rooms" to "details"
+--add notes column to jobs
+
+--do i need to add sessions table
+--do we need "requested" in the emp_jobs table
--- models/api.go
+++ models/api.go
@@ -92,20 +92,20 @@ type JobsDisplayRequest struct {
 }
 
 type JobResponse struct {
-	ID         int                    `db:"job_id" json:"id"`
-	Customer   GetCustomerResponse    `json:"customer"`
-	LoadAddr   Address                `json:"loadAddr"`
-	UnloadAddr Address                `json:"unloadAddr"`
-	StartTime  pgtype.Timestamp       `db:"start_time" json:"startTime"`
-	HoursLabor pgtype.Interval        `db:"hours_labor" json:"hoursLabor"`
-	Finalized  bool                   `db:"finalized" json:"finalized"`
-	Rooms      map[string]interface{} `db:"rooms" json:"rooms"`
-	Pack       bool                   `db:"pack" json:"pack"`
-	Unpack     bool                   `db:"unpack" json:"unpack"`
-	Load       bool                   `db:"load" json:"load"`
-	Unload     bool                   `db:"unload" json:"unload"`
-	Clean      bool                   `db:"clean" json:"clean"`
-	Milage     int                    `db:"milage" json:"milage"`
-	Cost       string                 `db:"cost" json:"cost"`
-	//AssignedEmp []Employee             `json:"assignedEmployees"`
+	ID          int                    `db:"job_id" json:"id"`
+	Customer    GetCustomerResponse    `json:"customer"`
+	LoadAddr    Address                `json:"loadAddr"`
+	UnloadAddr  Address                `json:"unloadAddr"`
+	StartTime   pgtype.Timestamp       `db:"start_time" json:"startTime"`
+	HoursLabor  pgtype.Interval        `db:"hours_labor" json:"hoursLabor"`
+	Finalized   bool                   `db:"finalized" json:"finalized"`
+	Rooms       map[string]interface{} `db:"rooms" json:"rooms"`
+	Pack        bool                   `db:"pack" json:"pack"`
+	Unpack      bool                   `db:"unpack" json:"unpack"`
+	Load        bool                   `db:"load" json:"load"`
+	Unload      bool                   `db:"unload" json:"unload"`
+	Clean       bool                   `db:"clean" json:"clean"`
+	Milage      int                    `db:"milage" json:"milage"`
+	Cost        string                 `db:"cost" json:"cost"`
+	AssignedEmp []GetEmployeeResponse  `json:"assignedEmployees"`
 }
